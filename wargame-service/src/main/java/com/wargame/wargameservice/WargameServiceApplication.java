2b49b22a0cb04f6985b7b0528ac34cb25c9a43c7
Adrian Milne <adrian.milne@crowncommercial.gov.uk>
2021-01-13 08:06:46 +0000

    Starting template created from Spring Initializr

---
+++ wargame-service/src/main/java/com/wargame/wargameservice/WargameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wargame.wargameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WargameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WargameServiceApplication.class, args);
+	}
+
+}
